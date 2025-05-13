d18c04aeebe8cf69af80baaf46507bb0492984c2
unknown <ebrahim.cse.bu@gmail.com>
2025-05-13 15:03:55 +0600

    mechanic job screen done

--- lib/pregentaitions/screens/mechanic/mechanic_job_screen/mechanic_job_screen.dart
+++ lib/pregentaitions/screens/mechanic/mechanic_job_screen/mechanic_job_screen.dart
@@ -8,7 +8,11 @@ import 'package:autorevive/pregentaitions/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_switch/flutter_switch.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../../../controllers/mechanic_controller/mechanic_job_controller.dart';
 
 class MechanicJobScreen extends StatefulWidget {
   const MechanicJobScreen({super.key});
@@ -18,132 +22,159 @@ class MechanicJobScreen extends StatefulWidget {
 }
 
 class _MechanicJobScreenState extends State<MechanicJobScreen> {
+  MechanicJobController mechanicJobController = Get.put(MechanicJobController());
+
   bool status = false;
-  double _miles = 4;
+  double _miles = 2;
+
+  @override
+  void initState() {
+    mechanicJobController.settingRadiusLimits().then((_) {
+      final min = mechanicJobController.radiusLimits.value.value?.min ?? 0;
+      setState(() {
+        _miles = min.toDouble();
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final radiusLimits = mechanicJobController.radiusLimits.value.value;
+    final double min = radiusLimits?.min?.toDouble() ?? 0;
+    final double max = radiusLimits?.max?.toDouble() ?? 100;
+
     return CustomScaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: CustomText(
-            maxline: 2,
-            text: "Job",
-            fontsize: 20.sp,
-          )),
-      body: Column(
-        children: [
-          SizedBox(height: 32.h),
+        centerTitle: true,
+        title: CustomText(
+          maxline: 2,
+          text: "Job",
+          fontsize: 20.sp,
+        ),
+      ),
+      body: Obx(() {
+        if (mechanicJobController.radiusLimits.value.value == null) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-          /// ==================  Available button ==================
-          CustomContainer(
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.8),
-                  offset: const Offset(0, 4),
-                  blurRadius: 4)
-            ],
-            paddingAll: 10.r,
-            radiusAll: 100.r,
-            color: AppColors.primaryShade300,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FlutterSwitch(
-                  width: 62.w,
-                  height: 32.h,
-                  toggleSize: 32.r,
-                  toggleColor: const Color(0xFF8A8AFF),
-                  activeColor: AppColors.switchColor,
-                  inactiveColor: AppColors.switchColor,
-                  value: status,
-                  borderRadius: 20.r,
-                  padding: 0,
-                  onToggle: (val) {
-                    setState(() {
-                      status = val;
-                    });
-                  },
-                ),
-                CustomText(
-                  text: 'Available Now',
-                  fontsize: 20.sp,
-                  color: Colors.white,
-                  left: 20.w,
-                )
+        return Column(
+          children: [
+            SizedBox(height: 32.h),
+
+            /// ==================  Available button ==================
+            CustomContainer(
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.8),
+                    offset: const Offset(0, 4),
+                    blurRadius: 4)
               ],
-            ),
-          ),
-          SizedBox(height: 24.h),
-
-          /// ==================  Filter button ==================
-
-          CustomContainer(
-            width: double.infinity,
-            radiusAll: 16.r,
-            paddingAll: 24.r,
-            color: AppColors.primaryShade300,
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.8),
-                  offset: const Offset(0, 4),
-                  blurRadius: 4)
-            ],
-            child: Column(
-              children: [
-                CustomText(
-                    text: 'Filter',
-                    fontsize: 22.sp,
-                    color: Colors.white,
-                    bottom: 8.h),
-                Align(
-                    alignment: Alignment.centerLeft,
-                    child: CustomText(
-                      left: 16.w,
-                      text: 'Miles From Me',
-                      fontsize: 20.sp,
-                      color: Colors.white,
-                    )),
-
-                CustomSlider(value: _miles,onChanged: (val){
-                  _miles = val;
-                  setState(() {
-
-                  });
-                }),
-                CustomText(
-                    text: 'Maximum',
-                    fontsize: 12.sp,
-                    color: Colors.white,
-                    bottom: 4.h),
-                CustomContainer(
-                  alignment: Alignment.center,
-                  radiusAll: 12.r,
-                  width: 95.w,
-                  height: 42.h,
-                  bordersColor: Colors.white,
-                  child: CustomText(
-                    text: _miles.toInt().toString(),
-                    color: Colors.white,
-                    fontsize: 16.sp,
+              paddingAll: 10.r,
+              radiusAll: 100.r,
+              color: AppColors.primaryShade300,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FlutterSwitch(
+                    width: 62.w,
+                    height: 32.h,
+                    toggleSize: 32.r,
+                    toggleColor: const Color(0xFF8A8AFF),
+                    activeColor: AppColors.switchColor,
+                    inactiveColor: AppColors.switchColor,
+                    value: status,
+                    borderRadius: 20.r,
+                    padding: 0,
+                    onToggle: (val) {
+                      setState(() {
+                        status = val;
+                      });
+                    },
                   ),
-                )
-              ],
+                  CustomText(
+                    text: 'Available Now',
+                    fontsize: 20.sp,
+                    color: Colors.white,
+                    left: 20.w,
+                  )
+                ],
+              ),
             ),
-          ),
+            SizedBox(height: 24.h),
 
-
-
-
-          const Spacer(),
-          CustomButton(
+            /// ==================  Filter button ==================
+            CustomContainer(
+              width: double.infinity,
+              radiusAll: 16.r,
+              paddingAll: 24.r,
+              color: AppColors.primaryShade300,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black.withOpacity(0.8),
+                    offset: const Offset(0, 4),
+                    blurRadius: 4)
+              ],
+              child: Column(
+                children: [
+                  CustomText(
+                      text: 'Filter',
+                      fontsize: 22.sp,
+                      color: Colors.white,
+                      bottom: 8.h),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: CustomText(
+                        left: 16.w,
+                        text: 'Miles From Me',
+                        fontsize: 20.sp,
+                        color: Colors.white,
+                      )),
+                  CustomSlider(
+                    value: _miles,
+                    min: min,
+                    max: max,
+                    onChanged: (val) {
+                      setState(() {
+                        _miles = val;
+                      });
+                    },
+                  ),
+                  CustomText(
+                      text: 'Max: ${max.toInt()}',
+                      fontsize: 12.sp,
+                      color: Colors.white,
+                      bottom: 4.h),
+                  CustomContainer(
+                    alignment: Alignment.center,
+                    radiusAll: 12.r,
+                    width: 95.w,
+                    height: 42.h,
+                    bordersColor: Colors.white,
+                    child: CustomText(
+                      text: _miles.toInt().toString(),
+                      color: Colors.white,
+                      fontsize: 16.sp,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const Spacer(),
+            CustomButton(
               title: 'Apply',
               onpress: () {
-                context.pushNamed(AppRoutes.mechanicJobRequestScreen);
-              },),
-          SizedBox(height: 20.h)
-        ],
-      ),
+                context.pushNamed(
+                  AppRoutes.mechanicJobRequestScreen,
+                  extra: _miles.toInt().toString(),
+                );
+              },
+            ),
+            SizedBox(height: 20.h)
+          ],
+        );
+      }),
     );
   }
 }
+
